453f33453298cef78d441680b420282a231c4363
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-02-20 15:21:06 +0530

    added support for mongo installation and configurations

--- index.js
+++ index.js
@@ -4,21 +4,13 @@ var path = require('path');
 exports.install = function(vars, taskListOptions) {
   var taskList = nodemiral.taskList("MongoDB Installation", taskListOptions);
 
-  //setting mongodb options
-  var mongoOptions = {
-    replSet: vars.replSet || "meteor"
-  };
-
-  if(vars.options) {
-    for(var key in vars.options) {
-      mongoOptions[key] = vars.options;
-    }
-  }
-
   // Installation
   taskList.executeScript('install', {
     script: path.resolve(__dirname, 'scripts/install.sh')
   });
+
+  taskList.copy('copy mongodb configuration', getConfigFileTask({auth: true}));
+  taskList.execute('restart mongodb', getRestartTask());
 
   // Create Admin User
   taskList.copy('copy script for admin user creation', {
@@ -31,10 +23,17 @@ exports.install = function(vars, taskListOptions) {
     script: path.resolve(__dirname, 'scripts/set_admin_user.sh')
   });
 
+  return taskList;
+};
+
+exports.configure = function(vars, taskListOptions) {
+  var taskList = nodemiral.taskList("MongoDB Installation", taskListOptions);
+  var mongoOptions = vars.options || {};
+
   //setting the keyFile
   if(vars.key) {
     mongoOptions['keyFile'] = "/opt/nodemiral/mongodb/key_file";
-    taskList.executeScript('create admin user', {
+    taskList.executeScript('create key file', {
       script: path.resolve(__dirname, 'scripts/set_key_file.sh'),
       vars: {
         key: vars.key
@@ -43,13 +42,7 @@ exports.install = function(vars, taskListOptions) {
   }
 
   //add production mongodb configuration
-  taskList.copy('copy mongodb configuration', {
-    src: path.resolve(__dirname, 'templates/mongodb.conf'),
-    dest: '/etc/mongodb.conf',
-    vars: {
-      options: mongoOptions
-    }
-  });
+  taskList.copy('copy mongodb configuration', getConfigFileTask(mongoOptions));
 
   //restart
   taskList.execute('restart mongodb', getRestartTask());
@@ -61,4 +54,26 @@ function getRestartTask() {
   return {
     command: "(sudo stop mongodb || :) && sudo start mongodb"
   };
+}
+
+function getConfigFileTask (options) {
+  options = options || {};
+  var mongoOptions = {
+    dbpath: '/opt/nodemiral/mongodb/db',
+    port: 27017,
+    auth: true,
+    nohttpinterface: true
+  };
+
+  for(var key in options) {
+    mongoOptions[key] = options[key];
+  }
+
+  return {
+    src: path.resolve(__dirname, 'templates/mongodb.conf'),
+    dest: '/etc/mongodb.conf',
+    vars: {
+      options: mongoOptions
+    }
+  }
 }
--- scripts/set_admin_user.sh
+++ scripts/set_admin_user.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#start mongodb
-sudo start mongodb
-
 # wait for mongo for 10 minutes (need big time for journal setup and for pre-allocate)
 wait-for-mongo mongodb://localhost:27017/admin 600000
 
