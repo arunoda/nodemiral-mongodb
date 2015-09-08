0876b98cedbca1e766cff955659e7d3524e888d4
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-09-08 11:52:26 +0530

    Fix the snapshot file

--- scripts/take_snapshot.sh
+++ scripts/take_snapshot.sh
@@ -5,6 +5,7 @@ sudo lvremove -f /dev/vgdata/snapdata || :
 sudo lvcreate -n snapdata -s -l 20%VG /dev/vgdata/vol0
 sudo mkdir -p /snapdata
 sudo mount /dev/vgdata/snapdata /snapdata
+cd /snapdata
 tar czf /backup/backup-`date +'%Y_%m_%d_%H'`.tar.gz ./
 cd /tmp
 umount /snapdata
